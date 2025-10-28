5232202a0b16b9048f1a1ef487bac287cd2bee4e
Srikanth Kamalla <srikanthkamalla2919@gmail.com>
2025-10-28 10:08:54 +0530

    updated resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -190,7 +190,7 @@ const Hero = () => {
           <div>
             <a
               className="btn btn-primary"
-              href="https://drive.google.com/file/d/1Qqdjb2XgNOlxYdZou2CUW_OQOFvlH5A7/view?usp=sharing"
+              href="https://drive.google.com/file/d/1GmMhiQV9UoMgVD9bwXkDfSDZf1tGPudR/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
             >
